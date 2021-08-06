b26d00091c0323d56d9933ab8eaf6587ea308187
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2021-08-06 18:37:57 -0500

    Animations

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -48,7 +48,8 @@ class Favorites extends Component {
                       text: "OK",
                       onPress: () => this.props.deleteFavorite(item.id),
                     },
-                  ]
+                  ],
+                  { cancelable: false }
                 )
               }
             >
